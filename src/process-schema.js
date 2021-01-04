542e74919a99a172d1784360e08dac691cf6f10e
Ivo Tzvetkov <ivotkv@gmail.com>
2021-01-04 16:43:19 +1100

    moved typeMap to constants.js

--- src/process-schema.js
+++ src/process-schema.js
@@ -1,29 +1,5 @@
 const { loadRawSchema, saveOutSchema } = require('./utils');
-
-const typeMap = Object.freeze({
-  // see: https://github.com/AntlerVC/firetable/blob/master/www/src/constants/fields.tsx
-  'autoNumber': 'NUMBER', // automatically generates a unique, automatically incremented number
-  'text': 'SIMPLE_TEXT',
-  'number': 'NUMBER',
-  'select': 'SINGLE_SELECT',
-  'multilineText': 'LONG_TEXT',
-  'multiSelect': 'MULTI_SELECT',
-  'richText': 'RICH_TEXT',
-  'foreignKey': 'JSON', // [ 'recxLter7QYPTgaKq' ],
-  'multipleAttachment': 'JSON', /*  [
-                                      {
-                                        id: 'att4eybld3qlhFfIK',
-                                        url: 'https://cdn.filestackcontent.com/F94oVb05QbmCZlMZeWeJ',
-                                        filename: 'Briser_le_code_document_enseignant.pdf',
-                                        type: 'application/pdf'
-                                      }
-                                    ] */
-  'lookup': 'JSON', // described in column.typeOptions, value is pre-calculated column.typeOptions.resultType
-  'date': 'DATE',
-  'rollup': 'JSON', // described in column.typeOptions, value is pre-calculated column.typeOptions.resultType
-  'checkbox': 'CHECK_BOX',
-  'formula': 'JSON' // described in column.typeOptions, value is pre-calculated column.typeOptions.resultType
-});
+const { typeMap } = require('./constants');
 
 const mapColumn = (column, index) => {
   return {
