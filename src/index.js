a9df4708e1a9f623fb3915f7888344b2da4e43ae
Ivo Tzvetkov <ivotkv@gmail.com>
2021-01-04 14:12:07 +1100

    added single run command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer');
-const { Command } = require("commander");
-const { version } = require("../package.json");
+const { Command } = require('commander');
+const { version } = require('../package.json');
 const program = new Command();
 program.version(version);
 
@@ -20,44 +20,74 @@ const { fetchTables } = require('./fetch-tables');
 const { importSchema } = require('./import-schema');
 const { importTables } = require('./import-tables');
 
-program
-  .command("fetch-schema")
-  .description("Fetch base schema from Airtable")
-  .action(async () => {
-    const answers = await inquirer.prompt([
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Airtable login email:'
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Airtable login password:'
-      }
-    ]);
-    await fetchSchema(getEnv('BASE_ID'), answers.email, answers.password);
-  });
+const runFetchSchema = async () => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Airtable login email:'
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Airtable login password:'
+    }
+  ]);
+  await fetchSchema(getEnv('BASE_ID'), answers.email, answers.password);
+};
 
 program
-  .command("fetch-tables")
-  .description("Fetch schema tables from Airtable")
-  .action(async () => {
-    await fetchTables(getEnv('BASE_ID'), getEnv('AIRTABLE_KEY'));
-  });
+  .command('fetch-schema')
+  .description('Fetch base schema from Airtable')
+  .action(runFetchSchema);
+
+const runFetchTables = async () => {
+  await fetchTables(getEnv('BASE_ID'), getEnv('AIRTABLE_KEY'));
+};
 
 program
-  .command("import-schema")
-  .description("Import schema into Firetable")
-  .action(async () => {
-    await importSchema(getEnv('BASE_ID'), firebase.firestore());
-  });
+  .command('fetch-tables')
+  .description('Fetch schema tables from Airtable')
+  .action(runFetchTables);
+
+const runImportSchema = async () => {
+  await importSchema(getEnv('BASE_ID'), firebase.firestore());
+};
 
 program
-  .command("import-tables")
-  .description("Import tables into Firetable")
-  .action(async () => {
-    await importTables(getEnv('BASE_ID'), firebase.firestore());
-  });
+  .command('import-schema')
+  .description('Import schema into Firetable')
+  .action(runImportSchema);
+
+const runImportTables = async () => {
+  await importTables(getEnv('BASE_ID'), firebase.firestore());
+};
+
+program
+  .command('import-tables')
+  .description('Import tables into Firetable')
+  .action(runImportTables);
+
+const confirmRun = async (fn, name) => {
+  const answers = await inquirer.prompt([{
+    type: 'confirm',
+    name: 'confirmed',
+    message: `Run ${name}?`,
+    default: true
+  }]);
+  return answers.confirmed && await fn();
+}
+
+const runDefault = async () => {
+  await confirmRun(runFetchSchema, 'fetch-schema');
+  await confirmRun(runFetchTables, 'fetch-tables');
+  await confirmRun(runImportSchema, 'import-schema');
+  await confirmRun(runImportTables, 'import-tables');
+};
+
+program
+  .command('run', { isDefault: true })
+  .description('Run the full migration')
+  .action(runDefault);
 
 program.parse(process.argv);
