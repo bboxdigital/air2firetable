08eeca2f4449d0c1ec91fb86836b8f7a6187405f
Ivo Tzvetkov <ivotkv@gmail.com>
2021-01-11 11:19:57 +1100

    cleaner format

--- src/process-schema.ts
+++ src/process-schema.ts
@@ -10,41 +10,33 @@ import {
 } from "./types/firetable";
 import { FiretableColumn } from "./types/firetable-columns";
 
-const mapColumn = (column: AirtableColumn, index: number): FiretableColumn => {
-  return {
-    config: {},
-    fieldName: column.name,
-    key: column.name,
-    name: column.name,
-    type: fieldMap[column.type],
-    index,
-  };
-};
+const mapColumn = (column: AirtableColumn, index: number): FiretableColumn => ({
+  config: {},
+  fieldName: column.name,
+  key: column.name,
+  name: column.name,
+  type: fieldMap[column.type],
+  index,
+});
 
-const getTableColumns = (table: AirtableTable): FiretableTableColumns => {
-  return Object.fromEntries(
-    table.columns.map((column, index) => [column.name, mapColumn(column, index)])
-  );
-};
+const getTableColumns = (table: AirtableTable): FiretableTableColumns => Object.fromEntries(
+  table.columns.map((column, index) => [column.name, mapColumn(column, index)])
+);
 
-const getTableSettings = (table: AirtableTable): FiretableTableSettings => {
-  return {
-    tableType: "primaryCollection",
-    collection: table.id,
-    isCollectionGroup: false,
-    name: table.name,
-    roles: ["ADMIN"],
-    description: table.defaultView.name,
-    section: "Default",
-  };
-};
+const getTableSettings = (table: AirtableTable): FiretableTableSettings => ({
+  tableType: "primaryCollection",
+  collection: table.id,
+  isCollectionGroup: false,
+  name: table.name,
+  roles: ["ADMIN"],
+  description: table.defaultView.name,
+  section: "Default",
+});
 
-const getTableSchema = (table: AirtableTable): FiretableTableSchema => {
-  return {
-    ...getTableSettings(table),
-    columns: getTableColumns(table),
-  };
-};
+const getTableSchema = (table: AirtableTable): FiretableTableSchema => ({
+  ...getTableSettings(table),
+  columns: getTableColumns(table),
+});
 
 export const processSchema = async (baseId: string) => {
   const airtableSchema: AirtableSchema = await loadFile(Prefix.Airtable, baseId);
