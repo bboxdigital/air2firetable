dc857296b277213e86bd8dccf4e59f4de42c4982
Ivo Tzvetkov <ivotkv@gmail.com>
2021-06-14 15:02:39 +1000

    puppeteer fixes

--- src/fetch-schema.ts
+++ src/fetch-schema.ts
@@ -4,14 +4,20 @@ import { AirtableSchema } from "./types/airtable";
 import { Prefix, saveFile } from "./utils";
 
 export const fetchSchema = async (baseId: string, email: string, password: string) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
   const page = await browser.newPage();
 
   await page.goto(`https://airtable.com/login?continue=/${baseId}/api/docs`);
   await page.waitForSelector("#sign-in-form-fields-root > div > label > div");
-  await page.type('#sign-in-form-fields-root > div > label > input[name="email"]', email);
-  await page.type('#sign-in-form-fields-root > div > label > input[name="password"]', password);
-  await page.click('#sign-in-form-fields-root > div > label > input[type="submit"]');
+  await page.type('#sign-in-form-fields-root > div > label > [name="email"]', email);
+  await page.type('#sign-in-form-fields-root > div > label > [name="password"]', password);
+  await page.click('#sign-in-form-fields-root > div > label > [type="submit"]');
 
   await page.waitForSelector(".docs > .languageTabs > .tab");
   const airtableSchema: AirtableSchema = await page.evaluate(getAirtableSchema);
